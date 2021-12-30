beb6d1796c39c519de9e869b9c5b8ee3b76d1454
virgknight <virginiaknight888@gmail.com>
2021-12-30 15:25:36 -0700

    Set up User and Session Controllers

--- app/models/user.rb
+++ app/models/user.rb
@@ -18,7 +18,7 @@ class User < ApplicationRecord
     validates :email, :username, :session_token, presence: true, uniqueness: true
     validates :password_digest, :first_name, :last_name, presence: true
     validates :password, length: {minimum: 6}, allow_nil: true
-    # later may want to add inclusion validation for pronouns
+    # VKNOTE: later may want to add inclusion validation for pronouns
     after_initialize :ensure_session_token
 
     attr_reader :password
