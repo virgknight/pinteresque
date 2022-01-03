7a4efdfff4bff3e0f8a4ad504aa6973cb1998cc8
virgknight <virginiaknight888@gmail.com>
2022-01-03 14:18:49 -0700

    Refine user errors on signup page

--- frontend/components/session/signup_form.jsx
+++ frontend/components/session/signup_form.jsx
@@ -19,12 +19,37 @@ class SignupForm extends React.Component {
         this.props.clearErrors();
     }
 
+    fixErrors (rawErrors) {
+        let newErrors = {email: null, password: null, age: null};
+        
+        rawErrors.forEach((error) =>{
+            if (error.includes("Email")) {
+                newErrors.email = (<p className="error">
+                    You missed a spot! Don't forget to add your email.
+                </p>);
+            } else if (error.includes("Password")) {
+                newErrors.password = (<p className="error">
+                    Oops! Passwords must be 6 characters or longer.
+                </p>);
+            } else if (error.includes("Age must be greater")) {
+                newErrors.age = (<p className="error">
+                    Sorry! You must be 13 years or older to use Pinteresque.
+                </p>);
+            } else if (error.includes("Age is not a number")) {
+                newErrors.age = (<p className="error">
+                    You missed a spot! Don't forget to add your age.
+                </p>);
+            }
+        })
+
+        return newErrors;
+    }
+
     handleSubmit(e) {
         e.preventDefault();
         let {email, age} = this.state;
         const newUserName = email.slice(0, email.indexOf('@'));
         const user = Object.assign({}, this.state, { age: parseInt(age), username: newUserName });
-        debugger;
         this.props.processForm(user).then(() =>
             this.props.history.push("/"));
     }
@@ -36,6 +61,7 @@ class SignupForm extends React.Component {
     }
 
     render() {
+        const errors = this.fixErrors(this.props.errors)
         let { email, password, age } = this.state;
         return (
             <div className="modal">
@@ -43,24 +69,26 @@ class SignupForm extends React.Component {
                 <div onClick={() => this.props.history.push("/")} className="close-button"><CloseIcon /></div>
                 <form>
                     <img src={window.round_logo} width="50" height="50" />
-                    <h2>Welcome to Pinteresque</h2>
-                    <p>Get tons of new ideas!</p>
+                    <h2>Unlimited free access to the world's best ideas</h2>
+                    <p>Sign up to see more!</p>
                     <br />
-                    {this.props.errors.map((error, i) => (<p key={`autherror-${i}`} className="error">{error}</p>))}
                     <input type="text"
                         placeholder="Email"
                         value={email}
                         onChange={this.handleInput('email')}></input>
+                    {errors.email}
                     <br />
                     <input type="password"
                         placeholder="Password"
                         value={password}
                         onChange={this.handleInput('password')}></input>
+                    {errors.password}
                     <br />
                     <input type="number"
                         placeholder="Age"
                         value={age}
                         onChange={this.handleInput('age')}></input>
+                    {errors.age}
                     <button className="modal-button"
                             onClick={this.handleSubmit}>Continue</button>
                 </form>
